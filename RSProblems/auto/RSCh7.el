30c1b33a127bb02bf4c3bf5b3d764cf23bf53126
Nooreen Dabbish <ndabbish@gmail.com>
2015-01-28 10:18:58 -0500

    additional files/folders

---
+++ RSProblems/auto/RSCh7.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ "RSCh7"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "soul"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "latexsym"
+    "amssymb"
+    "hyperref")
+   (TeX-add-symbols
+    '("alert" 1))
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-1-1"
+    "sec-1-1-1"
+    "sec-1-1-2"
+    "sec-1-1-3")))
+
