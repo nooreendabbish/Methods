ae0d19a3c30fb252cbd74883c6de6b7c5314d393
Nooreen Dabbish <ndabbish@gmail.com>
2015-02-19 14:20:53 -0500

    drafted

--- Homeworks/DabbishHW4a.R
+++ Homeworks/DabbishHW4a.R
@@ -23,6 +23,17 @@ library(MASS); library(xtable)
   V2[6,5] <- -1
   V2[5,6] <- -1
 
+
+#Variables from Problem 4 of HW3:
+data(Boston)
+Y_B = as.matrix(Boston$medv)
+X_B = as.matrix(Boston[,c('crim','nox','rm','age','dis')])
+X_B = cbind(rep(1,dim(Boston)[1]),X_B)
+bhat_B <- ginv(t(X_B)%*%X_B) %*% t(X_B) %*% Y_B
+Yhat_B <- X_B %*% bhat_B
+err_B <- Y_B - Yhat_B
+sigsqhat_B <- t(err_B) %*% err_B / (dim(X_B)[1] - qr(X_B)$rank)
+
 #Find V^(-1/2)
 Vh1 <-solve(V1^(1/2))
 
@@ -36,8 +47,8 @@ SSE <- t(U-Uhat) %*% (U-Uhat)
 
 qr(W)$rank
 
-lowerchi <- qchisq(.05, df=4)
-upperchi <- qchisq(.95, df=4)
+lowerchi <- qchisq(.05, df=(length(U) - qr(W)$rank))
+upperchi <- qchisq(.95, df=(length(U) - qr(W)$rank))
 
 SSE/lowerchi
 SSE/upperchi
@@ -55,5 +66,47 @@ SSE <- t(U-Uhat) %*% (U-Uhat)
 
 qr(W)$rank
 
-lowerchi <- qchisq(.05, df=4)
-upperchi <- qchisq(.95, df=4)
+lowerchi <- qchisq(.05, df=(length(U) - qr(W)$rank))
+upperchi <- qchisq(.95, df=(length(U) - qr(W)$rank))
+
+Yhat <- X %*% ginv(t(X) %*% X) %*% t(X) %*% Y
+
+SSE <- t(Y-Yhat) %*% (Y-Yhat)
+
+lowerchi <- qchisq(.05, df=(length(Y) - qr(X)$rank))
+upperchi <- qchisq(.95, df=(length(Y) -qr(X)$rank))
+
+#Find the t distribution quantile
+t_1b <- qt(.05, (length(Y) - qr(W)$rank - 1) )
+
+a_1b = matrix(c(1,0,1,0,0))
+s_1b <- sqrt(SSE/(length(Y) - qr(W)$rank - 1))
+Bhat_1b <- ginv(t(W) %*% W) %*% t(W) %*% U
+quad_1b <- sqrt(t(a_1b) %*% ginv(t(W)%*%W) %*% a_1b)
+upper1b <- t(a_1b) %*% Bhat_1b - t_1b * s_1b * quad_1b
+lower1b <- t(a_1b) %*% Bhat_1b + t_1b * s_1b * quad_1b
+
+a_1c = matrix(c(0,1,-1,0,0))
+
+quad_1c <- sqrt(t(a_1c) %*% ginv(t(W)%*%W) %*% a_1c)
+upper1c <- t(a_1c) %*% Bhat_1b - t_1b * s_1b * quad_1c
+lower1c <- t(a_1c) %*% Bhat_1b + t_1b * s_1b * quad_1c
+
+SSH <- t(t(a_1c) %*% Bhat_1b) %*% ginv(t(a_1c)%*%ginv(t(W)%*%W)%*%a_1c)%*%t(a_1c)%*%Bhat_1b
+
+p_1d <- pf(SSH/SSE, 1, 1, lower.tail=FALSE)
+
+#Find SSR in the full model.
+SSR_Bf <- t(bhat_B) %*% t(X_B) %*% Y_B - (length(Y_B)*(mean(Y_B))^2)
+
+#create reduced model design matric and X1_B and estimator bhat1_B
+X1_B <- X_B[,-c(3,5)]
+bhat1_B <- ginv(t(X1_B)%*%X1_B) %*% t(X1_B) %*% Y_B
+SSR_Br <- t(bhat1_B) %*% t(X1_B) %*% Y_B - (length(Y_B)*(mean(Y_B))^2)
+
+SSE_B <- t(Y_B)%*%Y_B - t(bhat_B)%*%t(X_B)%*%Y_B
+
+F_2f <- ((SSR_Bf - SSR_Br)/2)/(SSE_B/(length(Y_B) - qr(X_B)$rank))
+
+pf_2f <- pf(F_2f, 2, (length(Y_B)-(qr(X_B)$rank)), lower.tail=F)
+pf_2f
